0b576dacf3d4f943356d6357abe08ce476621c1f
Onur <onurbelek@outlook.com>
2023-05-16 14:22:07 +0100

    simple api setup

--- main.go
+++ main.go
@@ -1,61 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"github.com/gin-gonic/gin"
 )
 
+type incubator struct {
+	Microplates        string `json:microplates`
+	TempRange          string `json:tempRange`
+	HumidityRange      string `json:humidityRange`
+	IncubatorPlacement string `json:incubatorPlacement`
+	ExternalControlBox string `json:externalControlBox`
+}
+
+// setup router and handle mainpoints of API
 func main() {
-	fmt.Println("Hello There!")
-
-	var name string = "Onur Belek"
-	var status string = "working hard!"
-	var weeklyGymCount int = 4
-
-	fmt.Printf("%v is %v\nHe went to gym %v times this week\nHis favourite variable is %T\n", name, status, weeklyGymCount, weeklyGymCount)
-
-	// PRINTING POINTERS
-	fmt.Println(&name)
-
-	// ARRAYS
-	var randomArrNumbers = [50]int{10, 20, 30, 40, 999, 115, 666}
-
-	// [50] is size of arr, int is data type, {} is the actual array you initialise and it can be empty
-
-	// strongly typed like java - shows compiler errors like java too pretty nice
-
-	fmt.Println(randomArrNumbers)
-
-	var bookings [5]string
-	// can add new elements to array with index location
-	bookings[0] = name
-	bookings[1] = "Donald Trump"
-	bookings[2] = "Franklin Roosevelt"
-	bookings[3] = "Herbert Hoover"
-	bookings[4] = "Joe Biden"
-
-	fmt.Println(len(bookings)) // len(arr) returns length of array like python
-
-	// SLICES - abstraction of array - dynamic and powerful, index-based resized when needed - good for when you dont know what the length of the array will be
-
-	var bookingSlice []string // don't need to define size
-
-	bookingSlice = append(bookingSlice, "Donald Trump") // adding a value to the slice
-
-	fmt.Println(bookingSlice)
-
-	// FOR LOOPS - THIS IS AN INFINITE LOOP!
-	// for {
-	// 	fmt.Println(name)
-	// }
-
-	// for loops - iterate through array
-	firstNames := []string{}
-
-	for _, booking := range bookings { // range iterates over elements _ - is a blank variable to ignore variable you dont use
-		var names = strings.Fields(booking) // .split
-		firstNames = append(firstNames, names[0])
-	}
-
-	fmt.Println(firstNames)
+	router := gin.Default()
+	router.Run("localhost:8080")
 }
